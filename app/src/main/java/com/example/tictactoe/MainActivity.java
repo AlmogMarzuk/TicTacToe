319e6c3aebd294604d27788c7ba6cb173160c217
almog <almogmarzuk@gmail.com>
2022-12-06 22:53:35 +0200

    update logicGame 05/12/22

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView playXO;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
